49b3a9f1a9b81392802b45a6c719705958aec5cf
Kear Ibale <kdwest62@gmail.com>
2026-09-19 14:02:39 +0800

    Implement user menu for CSV operations
    Added a menu for user options to print CSV or count missing values.

--- missing-values.py
+++ missing-values.py
@@ -2,4 +2,16 @@ import pandas as pd
 
 path = input('CSV path: ')
 df = pd.read_csv(path)
-print(df.to_string())
+print('1. Print CSV\n2. Count Missing Values (Column)\n3. Exit')
+while True:
+    option = input('Select option: ')
+    if option == '1':
+        print(df.to_string())
+    elif option == '2':
+        output = df.isnull().sum()
+        print(output)
+    elif option == '3':
+        print('Program closed')
+        break
+    else:
+        print('Invalid input')
